0bae81e35b42ca411ba2743415c03c50e865c660
HuangMing17 <minhvip174@gmail.com>
2025-02-27 10:19:04 +0700

    [add] Lab01 ex01 ex02

---
+++ Lab01/ex01.py
@@ -0,0 +1,5 @@
+#nhập tên và tuổi từ ngươu dùng và in
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello", name)
+print("You are", age, "years old")
---
+++ Lab01/ex02.py
@@ -0,0 +1,6 @@
+# nhập bán kính từ người dùng 
+bankinh = input("Nhập bán kính hình tròn: ")
+# tính diện tích và chu vi
+dientich = 3.14*(int(bankinh)**2)
+#in diện tích cuar hình tròn
+print("Diện tích hình tròn là: ", dientich)
